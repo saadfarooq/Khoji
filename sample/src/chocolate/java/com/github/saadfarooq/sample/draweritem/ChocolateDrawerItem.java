2257c12038e302510df160f6b212b54176d376f5
Saad Farooq <sa@dfarooq.com>
2017-03-29 17:46:32 -0400

    Fixed an issue with parametrized dependency fields generation (#1)

--- sample/src/chocolate/java/com/github/saadfarooq/sample/draweritem/ChocolateDrawerItem.java
+++ sample/src/chocolate/java/com/github/saadfarooq/sample/draweritem/ChocolateDrawerItem.java
@@ -4,9 +4,14 @@ import com.github.saadfarooq.khoji.KhojiTarget;
 
 @KhojiTarget
 public class ChocolateDrawerItem implements DrawerItem {
+    private final String name;
+
+    public ChocolateDrawerItem(String name) {
+        this.name = name;
+    }
 
     @Override
     public String getTitle() {
-        return "Chocolate";
+        return name;
     }
 }
